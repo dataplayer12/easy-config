32bf3acb0ab87faa58793fcea18201548403fc29
dataplayer12 <jaiyamsharma@gmail.com>
2023-02-01 10:58:09 +0900

    map right super key also

--- setup.sh
+++ setup.sh
@@ -7,4 +7,6 @@ cp .tmux.conf ~/
 cp .xmodmap ~/
 #Reference: https://askubuntu.com/questions/137172/how-to-remap-super-left-key-to-control-key
 
+xmodmap ~/.xmodmap
+
 echo "All done! Happy programming :)"
