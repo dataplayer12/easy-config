e9d0fb4f10d618811a2a9670426d06bb5df65210
dataplayer12 <jaiyam.sharma@rapyuta-robotics.com>
2023-05-09 17:53:04 +0900

    easy gpu monitoring

--- setup.sh
+++ setup.sh
@@ -9,4 +9,17 @@ cp .xmodmap ~/
 
 xmodmap ~/.xmodmap
 
+
+if [[ $(uname -m) == *"x86"* ]]; then
+  echo 'alias gpu="watch -n 1 nvidia-smi"' >> ~/.bashrc
+elif [[ $(uname -m) == *"aarch64"* ]]; then
+  echo 'alias gpu="jtop"' >> ~/.bashrc
+# If the system architecture is neither x86 nor ARM (aarch64), print an error message
+else
+  echo "Error: Unsupported system architecture"
+  exit 1
+fi
+
+source ~/.bashrc
+
 echo "All done! Happy programming :)"
